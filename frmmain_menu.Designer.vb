7c186fee04004fa60cda416bc7c85f60a3e9d6cd
Nolan Garrett <N.Garrett.XIII@gmail.com>
2020-03-02 19:39:52 -0500

    working db view

--- frmmain_menu.Designer.vb
+++ frmmain_menu.Designer.vb
@@ -22,6 +22,7 @@ Partial Class frmmain_menu
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmmain_menu))
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.addApplicant = New System.Windows.Forms.Button()
@@ -30,15 +31,31 @@ Partial Class frmmain_menu
         Me.repairs = New System.Windows.Forms.Button()
         Me.settings = New System.Windows.Forms.Button()
         Me.userName = New System.Windows.Forms.Label()
+        Me.BindingNavigatorAddNewItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMovePreviousItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorPositionItem = New System.Windows.Forms.ToolStripTextBox()
+        Me.BindingNavigatorMoveFirstItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel()
+        Me.BindingNavigatorSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorMoveNextItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMoveLastItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorDeleteItem = New System.Windows.Forms.ToolStripButton()
+        Me.All_table_vwBindingNavigatorSaveItem = New System.Windows.Forms.ToolStripButton()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.All_table_vwBindingSource = New System.Windows.Forms.BindingSource(Me.components)
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.All_table_vwBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PictureBox1
         '
         Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
         Me.PictureBox1.Image = Global.DatabaseTestApplication2.My.Resources.Resources.CV_HB_Logo_White_400px
-        Me.PictureBox1.Location = New System.Drawing.Point(13, 29)
-        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.PictureBox1.Location = New System.Drawing.Point(13, 13)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(353, 127)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
@@ -48,7 +65,7 @@ Partial Class frmmain_menu
         'addApplicant
         '
         Me.addApplicant.Location = New System.Drawing.Point(20, 183)
-        Me.addApplicant.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.addApplicant.Margin = New System.Windows.Forms.Padding(4)
         Me.addApplicant.Name = "addApplicant"
         Me.addApplicant.Size = New System.Drawing.Size(165, 30)
         Me.addApplicant.TabIndex = 2
@@ -58,7 +75,7 @@ Partial Class frmmain_menu
         'editSurvey
         '
         Me.editSurvey.Location = New System.Drawing.Point(20, 220)
-        Me.editSurvey.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.editSurvey.Margin = New System.Windows.Forms.Padding(4)
         Me.editSurvey.Name = "editSurvey"
         Me.editSurvey.Size = New System.Drawing.Size(165, 30)
         Me.editSurvey.TabIndex = 3
@@ -68,7 +85,7 @@ Partial Class frmmain_menu
         'tracking
         '
         Me.tracking.Location = New System.Drawing.Point(20, 257)
-        Me.tracking.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.tracking.Margin = New System.Windows.Forms.Padding(4)
         Me.tracking.Name = "tracking"
         Me.tracking.Size = New System.Drawing.Size(165, 30)
         Me.tracking.TabIndex = 5
@@ -79,7 +96,7 @@ Partial Class frmmain_menu
         '
         Me.repairs.BackColor = System.Drawing.Color.Transparent
         Me.repairs.Location = New System.Drawing.Point(20, 294)
-        Me.repairs.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.repairs.Margin = New System.Windows.Forms.Padding(4)
         Me.repairs.Name = "repairs"
         Me.repairs.Size = New System.Drawing.Size(165, 30)
         Me.repairs.TabIndex = 6
@@ -89,7 +106,7 @@ Partial Class frmmain_menu
         'settings
         '
         Me.settings.Location = New System.Drawing.Point(20, 699)
-        Me.settings.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.settings.Margin = New System.Windows.Forms.Padding(4)
         Me.settings.Name = "settings"
         Me.settings.Size = New System.Drawing.Size(165, 30)
         Me.settings.TabIndex = 9
@@ -107,6 +124,122 @@ Partial Class frmmain_menu
         Me.userName.TabIndex = 10
         Me.userName.Text = "GET USER"
         '
+        'BindingNavigatorAddNewItem
+        '
+        Me.BindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorAddNewItem.Enabled = False
+        Me.BindingNavigatorAddNewItem.Image = CType(resources.GetObject("BindingNavigatorAddNewItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorAddNewItem.Name = "BindingNavigatorAddNewItem"
+        Me.BindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorAddNewItem.Size = New System.Drawing.Size(29, 24)
+        Me.BindingNavigatorAddNewItem.Text = "Add new"
+        '
+        'BindingNavigatorMovePreviousItem
+        '
+        Me.BindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMovePreviousItem.Enabled = False
+        Me.BindingNavigatorMovePreviousItem.Image = CType(resources.GetObject("BindingNavigatorMovePreviousItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMovePreviousItem.Name = "BindingNavigatorMovePreviousItem"
+        Me.BindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMovePreviousItem.Size = New System.Drawing.Size(29, 26)
+        Me.BindingNavigatorMovePreviousItem.Text = "Move previous"
+        '
+        'BindingNavigatorPositionItem
+        '
+        Me.BindingNavigatorPositionItem.AccessibleName = "Position"
+        Me.BindingNavigatorPositionItem.AutoSize = False
+        Me.BindingNavigatorPositionItem.Enabled = False
+        Me.BindingNavigatorPositionItem.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.BindingNavigatorPositionItem.Name = "BindingNavigatorPositionItem"
+        Me.BindingNavigatorPositionItem.Size = New System.Drawing.Size(50, 27)
+        Me.BindingNavigatorPositionItem.Text = "0"
+        Me.BindingNavigatorPositionItem.ToolTipText = "Current position"
+        '
+        'BindingNavigatorMoveFirstItem
+        '
+        Me.BindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveFirstItem.Image = CType(resources.GetObject("BindingNavigatorMoveFirstItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveFirstItem.Name = "BindingNavigatorMoveFirstItem"
+        Me.BindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveFirstItem.Size = New System.Drawing.Size(29, 26)
+        Me.BindingNavigatorMoveFirstItem.Text = "Move first"
+        '
+        'BindingNavigatorSeparator
+        '
+        Me.BindingNavigatorSeparator.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator.Size = New System.Drawing.Size(6, 29)
+        '
+        'BindingNavigatorCountItem
+        '
+        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
+        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(45, 26)
+        Me.BindingNavigatorCountItem.Text = "of {0}"
+        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
+        '
+        'BindingNavigatorSeparator1
+        '
+        Me.BindingNavigatorSeparator1.Name = "BindingNavigatorSeparator1"
+        Me.BindingNavigatorSeparator1.Size = New System.Drawing.Size(6, 29)
+        '
+        'BindingNavigatorMoveNextItem
+        '
+        Me.BindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveNextItem.Image = CType(resources.GetObject("BindingNavigatorMoveNextItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveNextItem.Name = "BindingNavigatorMoveNextItem"
+        Me.BindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveNextItem.Size = New System.Drawing.Size(29, 26)
+        Me.BindingNavigatorMoveNextItem.Text = "Move next"
+        '
+        'BindingNavigatorMoveLastItem
+        '
+        Me.BindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveLastItem.Image = CType(resources.GetObject("BindingNavigatorMoveLastItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveLastItem.Name = "BindingNavigatorMoveLastItem"
+        Me.BindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveLastItem.Size = New System.Drawing.Size(29, 26)
+        Me.BindingNavigatorMoveLastItem.Text = "Move last"
+        '
+        'BindingNavigatorSeparator2
+        '
+        Me.BindingNavigatorSeparator2.Name = "BindingNavigatorSeparator2"
+        Me.BindingNavigatorSeparator2.Size = New System.Drawing.Size(6, 29)
+        '
+        'BindingNavigatorDeleteItem
+        '
+        Me.BindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorDeleteItem.Image = CType(resources.GetObject("BindingNavigatorDeleteItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorDeleteItem.Name = "BindingNavigatorDeleteItem"
+        Me.BindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorDeleteItem.Size = New System.Drawing.Size(29, 26)
+        Me.BindingNavigatorDeleteItem.Text = "Delete"
+        '
+        'All_table_vwBindingNavigatorSaveItem
+        '
+        Me.All_table_vwBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.All_table_vwBindingNavigatorSaveItem.Enabled = False
+        Me.All_table_vwBindingNavigatorSaveItem.Image = CType(resources.GetObject("All_table_vwBindingNavigatorSaveItem.Image"), System.Drawing.Image)
+        Me.All_table_vwBindingNavigatorSaveItem.Name = "All_table_vwBindingNavigatorSaveItem"
+        Me.All_table_vwBindingNavigatorSaveItem.Size = New System.Drawing.Size(29, 26)
+        Me.All_table_vwBindingNavigatorSaveItem.Text = "Save Data"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.AllowUserToOrderColumns = True
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(373, 13)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(1175, 724)
+        Me.DataGridView1.TabIndex = 11
+        '
+        'All_table_vwBindingSource
+        '
+        Me.All_table_vwBindingSource.DataSource = GetType(DatabaseTestApplication2.all_table_vw)
+        '
         'frmmain_menu
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
@@ -114,7 +247,8 @@ Partial Class frmmain_menu
         Me.BackColor = System.Drawing.SystemColors.Control
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(1560, 738)
+        Me.ClientSize = New System.Drawing.Size(1560, 749)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.userName)
         Me.Controls.Add(Me.settings)
         Me.Controls.Add(Me.repairs)
@@ -123,10 +257,12 @@ Partial Class frmmain_menu
         Me.Controls.Add(Me.addApplicant)
         Me.Controls.Add(Me.PictureBox1)
         Me.DoubleBuffered = True
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "frmmain_menu"
         Me.Text = "MainMenu"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.All_table_vwBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -139,4 +275,18 @@ Partial Class frmmain_menu
     Friend WithEvents repairs As Button
     Friend WithEvents settings As Button
     Friend WithEvents userName As Label
+    Friend WithEvents BindingNavigatorAddNewItem As ToolStripButton
+    Friend WithEvents BindingNavigatorCountItem As ToolStripLabel
+    Friend WithEvents BindingNavigatorDeleteItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMoveFirstItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMovePreviousItem As ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator As ToolStripSeparator
+    Friend WithEvents BindingNavigatorPositionItem As ToolStripTextBox
+    Friend WithEvents BindingNavigatorSeparator1 As ToolStripSeparator
+    Friend WithEvents BindingNavigatorMoveNextItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMoveLastItem As ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator2 As ToolStripSeparator
+    Friend WithEvents All_table_vwBindingNavigatorSaveItem As ToolStripButton
+    Friend WithEvents All_table_vwBindingSource As BindingSource
+    Friend WithEvents DataGridView1 As DataGridView
 End Class
--- frmmain_menu.vb
+++ frmmain_menu.vb
@@ -1,14 +1,43 @@
-﻿Public Class frmmain_menu
+﻿Imports System.ComponentModel
+Imports System.Data.Entity.Core.Objects
+Imports System.Data.Entity
+Imports MySql.Data.MySqlClient
+Public Class frmmain_menu
+
+    Dim connection As New MySqlConnection("datasource=localhost;port=3306;username=root;password=root;database=mydb")
+
+
     Public Property usersName As String
 
-    Private Sub frmmain_menu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+
+    Private Sub frmmain_menu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         userName.Text = usersName
 
         'test
+
+        Dim SDA As New MySqlDataAdapter
+        Dim dbDataset As New DataTable
+        Dim bsource As New BindingSource
+        connection.Open()
+            Dim Query As String
+            Query = "select * from mydb.all_table_vw"
+        Dim Command2 As New MySqlCommand(Query, connection)
+        SDA.SelectCommand = Command2
+        SDA.Fill(dbDataset)
+            bsource.DataSource = dbDataset
+            DataGridView1.DataSource = bsource
+        SDA.Update(dbDataset)
+        connection.Close()
+
+
+
+
+
     End Sub
 
     Private Sub addApplicant_Click(sender As Object, e As EventArgs) Handles addApplicant.Click
         DatabaseTestApplication2.frmgeneral.Show()
     End Sub
 End Class
+
